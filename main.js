c5fc6b69c3b133326ade3ae28e67d9224edd85ec
이하영 <2hayoung@ihayeong-ui-MacBook-Air.local>
2021-07-26 21:14:43 +0900

    JavaScript: Display result if click playBtn using invisible class, HTML: Add a playBtn container

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 // 1. 플레이 버튼을 누르면 게임 스탑 버튼으로 바뀌기
 const playBtn = document.querySelector(".playBtn");
 const timeCounter = document.querySelector(".timeCounter");
+const numCounter = document.querySelector(".numCounter");
+const result = document.querySelector(".result");
+const resultTitle = document.querySelector(".result__title");
+const replayBtn = document.querySelector(".replayBtn");
 
 let time;
 let timer;
+let num;
 
 playBtn.addEventListener("click", () => {
   if (
+    //   처음 시작
     playBtn.classList.contains("start") &&
     playBtn.classList.contains("init")
   ) {
@@ -21,23 +27,30 @@ playBtn.addEventListener("click", () => {
     <i class="fas fa-stop"></i>
     `;
 
-    // 타이머 세팅
+    // timeCounter 세팅
     time = 10;
     playBtn.classList.remove("init");
     timeCounter.textContent = `0:${time--}`;
     timer = setInterval(() => {
       if (time === 0) {
         clearInterval(timer);
+        playBtn.classList.add("invisible");
+        resultTitle.textContent = "YOU LOSE";
+        result.classList.remove("invisible");
+        playBtn.classList.add("init");
+
+        // 여기에 YOU LOSE 나타나도록 작성
       }
       timeCounter.textContent = `0:${time--}`;
     }, 1000);
 
-    // setTimeout(() => {
-    //   clearInterval(timer);
-    // }, 10000);
-  } else if (playBtn.classList.contains("stop")) {
-    console.log(2);
-
+    // numCounter 세팅
+    num = 10;
+    numCounter.textContent = num;
+  } else if (
+    // 일시정지
+    playBtn.classList.contains("stop")
+  ) {
     playBtn.classList.remove("stop");
     playBtn.classList.add("start");
     playBtn.innerHTML = `
@@ -45,12 +58,14 @@ playBtn.addEventListener("click", () => {
     `;
 
     clearInterval(timer);
+    resultTitle.textContent = "REPLAY?";
+    result.classList.remove("invisible");
   } else if (
     playBtn.classList.contains("start") &&
     !playBtn.classList.contains("init")
   ) {
-    console.log(3);
-
+    //   일시정지에서 재개
+    result.classList.add("invisible");
     playBtn.classList.remove("start");
     playBtn.classList.add("stop");
 
@@ -61,40 +76,13 @@ playBtn.addEventListener("click", () => {
     timer = setInterval(() => {
       if (time === 0) {
         clearInterval(timer);
+        playBtn.classList.add("invisible");
+        resultTitle.textContent = "YOU LOSE";
+        result.classList.remove("invisible");
+        playBtn.classList.add("init");
+        // 여기에 YOU LOSE 나타나도록 작성
       }
       timeCounter.textContent = `0:${time--}`;
     }, 1000);
-
-    // setTimeout(() => {
-    //   clearInterval(timer);
-    // }, (time + 1) * 1000);
   }
 });
-
-// 2. 플레이 버튼을 누르면 카운터를 1초씩 줄이기, 스탑 버튼을 누르면 카운터를 멈추고 replay? 화면 보여주기
-
-// playBtn.addEventListener("click", () => {
-//   if (playBtn.classList.contains("init")) {
-//     time = 10;
-//     playBtn.classList.remove("init");
-//     timeCounter.textContent = `0:${time--}`;
-//     timer = setInterval(() => {
-//       timeCounter.textContent = `0:${time--}`;
-//     }, 1000);
-//     // setTimeout(() => {
-//     //   clearInterval(timer);
-//     // }, 10000);
-//   } else if (playBtn.classList.contains("start")) {
-//     clearInterval(timer);
-//   } else if (
-//     playBtn.classList.contains("stop") &&
-//     !playBtn.classList.contains("init")
-//   ) {
-//     timer = setInterval(() => {
-//       timeCounter.textContent = `0:${time--}`;
-//     }, 1000);
-//     setTimeout(() => {
-//       clearInterval(timer);
-//     }, (time + 1) * 1000);
-//   }
-// });
