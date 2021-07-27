21927d8f22bd383c3c36a6d5598bf1ffc88fe264
이하영 <2hayoung@ihayeong-ui-MacBook-Air.local>
2021-07-27 15:37:13 +0900

    JavaScript: Group replayBtn event and playBtn by initSet()

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const playBtn = document.querySelector(".playBtn");
 const timeCounter = document.querySelector(".timeCounter");
 const numCounter = document.querySelector(".numCounter");
 const resultContainer = document.querySelector(".result-container");
-const result = document.querySelector(".result");
 const resultTitle = document.querySelector(".result__title");
 const replayBtn = document.querySelector(".replayBtn");
 
@@ -17,115 +16,19 @@ let timer;
 let num;
 
 addEventListener("load", () => {
-  // playBtn 이벤트
-  playBtn.addEventListener("click", () => {
-    if (
-      //   처음 시작
-      playBtn.classList.contains("start") &&
-      playBtn.classList.contains("init")
-    ) {
-      //   class 세팅
-      playBtn.classList.remove("init");
-      playBtn.classList.remove("start");
-      playBtn.classList.add("stop");
-      field.classList.remove("invisible");
-
-      // html 세팅
-      playBtn.innerHTML = `
-      <i class="fas fa-stop"></i>
-      `;
-
-      // timeCounter 세팅
-      time = 10;
-      timeCounter.textContent = `0:${time--}`;
-      timer = setInterval(() => {
-        if (time === 0) {
-          clearInterval(timer);
-          playBtn.classList.add("invisible");
-          resultTitle.textContent = "YOU LOSE";
-          resultContainer.classList.remove("invisible");
-          playBtn.classList.add("init");
-        }
-        timeCounter.textContent = `0:${time--}`;
-      }, 1000);
-
-      // numCounter 세팅
-      num = 10;
-      numCounter.textContent = num;
-
-      // 당근, 벌레 세팅
-      const fieldWidth = field.offsetWidth;
-      const fieldHeight = field.offsetHeight;
-      const carrotWidth = carrot.width;
-      const carrotHeight = carrot.height;
-      const bugWidth = bug.width;
-      const bugHeight = bug.height;
-
-      carrots.forEach(carrot => {
-        carrot.style.transform = `translate(${
-          Math.random() * (fieldWidth - carrotWidth)
-        }px, ${Math.random() * (fieldHeight - carrotHeight)}px)`;
-      });
-
-      bugs.forEach(bug => {
-        bug.style.transform = `translate(${
-          Math.random() * (fieldWidth - bugWidth)
-        }px, ${Math.random() * (fieldHeight - bugHeight)}px)`;
-      });
-
-      field.classList.remove("invisible");
-    } else if (
-      // 일시정지
-      playBtn.classList.contains("stop")
-    ) {
-      playBtn.classList.remove("stop");
-      playBtn.classList.add("start");
-      playBtn.innerHTML = `
-      <i class="fas fa-play"></i>
-      `;
-
-      clearInterval(timer);
-      resultTitle.textContent = "REPLAY?";
-      resultContainer.classList.remove("invisible");
-    } else if (
-      playBtn.classList.contains("start") &&
-      !playBtn.classList.contains("init")
-    ) {
-      //   일시정지에서 재개
-      resultContainer.classList.add("invisible");
-      playBtn.classList.remove("start");
-      playBtn.classList.add("stop");
-
-      playBtn.innerHTML = `
-      <i class="fas fa-stop"></i>
-      `;
-
-      timer = setInterval(() => {
-        if (time === 0) {
-          clearInterval(timer);
-          playBtn.classList.add("invisible");
-          resultTitle.textContent = "YOU LOSE";
-          resultContainer.classList.remove("invisible");
-          playBtn.classList.add("init");
-          // 여기에 YOU LOSE 나타나도록 작성
-        }
-        timeCounter.textContent = `0:${time--}`;
-      }, 1000);
-    }
-  });
-
-  // replayBtn 이벤트
-  replayBtn.addEventListener("click", () => {
+  function initSet() {
     //   class 세팅
+    playBtn.classList.remove("init");
     playBtn.classList.remove("invisible");
-    resultContainer.classList.add("invisible");
     playBtn.classList.remove("start");
     playBtn.classList.add("stop");
+    field.classList.remove("invisible");
+    resultContainer.classList.add("invisible");
 
-    // html 세팅
+    // playBtn 세팅
     playBtn.innerHTML = `
-          <i class="fas fa-stop"></i>
-          `;
+        <i class="fas fa-stop"></i>
+        `;
 
     // timeCounter 세팅
     time = 10;
@@ -141,11 +44,11 @@ addEventListener("load", () => {
       timeCounter.textContent = `0:${time--}`;
     }, 1000);
 
-    //   numCounter 세팅
+    // numCounter 세팅
     num = 10;
-    numCounter.textContent = `${num}`;
+    numCounter.textContent = num;
 
-    //   당근, 벌레 재배치
+    // 당근, 벌레 세팅
     const fieldWidth = field.offsetWidth;
     const fieldHeight = field.offsetHeight;
     const carrotWidth = carrot.width;
@@ -166,6 +69,153 @@ addEventListener("load", () => {
         Math.random() * (fieldWidth - bugWidth)
       }px, ${Math.random() * (fieldHeight - bugHeight)}px)`;
     });
+  }
+
+  // playBtn 이벤트
+  playBtn.addEventListener("click", () => {
+    if (
+      //   처음 시작
+
+      playBtn.classList.contains("start") &&
+      playBtn.classList.contains("init")
+    ) {
+      initSet();
+      //   //   class 세팅
+      //   playBtn.classList.remove("init");
+      //   playBtn.classList.remove("start");
+      //   playBtn.classList.add("stop");
+      //   field.classList.remove("invisible");
+
+      //   // playBtn 세팅
+      //   playBtn.innerHTML = `
+      //     <i class="fas fa-stop"></i>
+      //     `;
+
+      //   // timeCounter 세팅
+      //   time = 10;
+      //   timeCounter.textContent = `0:${time--}`;
+      //   timer = setInterval(() => {
+      //     if (time === 0) {
+      //       clearInterval(timer);
+      //       playBtn.classList.add("invisible");
+      //       resultTitle.textContent = "YOU LOSE";
+      //       resultContainer.classList.remove("invisible");
+      //       playBtn.classList.add("init");
+      //     }
+      //     timeCounter.textContent = `0:${time--}`;
+      //   }, 1000);
+
+      //   // numCounter 세팅
+      //   num = 10;
+      //   numCounter.textContent = num;
+
+      //   // 당근, 벌레 세팅
+      //   const fieldWidth = field.offsetWidth;
+      //   const fieldHeight = field.offsetHeight;
+      //   const carrotWidth = carrot.width;
+      //   const carrotHeight = carrot.height;
+      //   const bugWidth = bug.width;
+      //   const bugHeight = bug.height;
+
+      //   carrots.forEach(carrot => {
+      //     carrot.style.transform = `translate(${
+      //       Math.random() * (fieldWidth - carrotWidth)
+      //     }px, ${Math.random() * (fieldHeight - carrotHeight)}px)`;
+      //   });
+
+      //   bugs.forEach(bug => {
+      //     bug.style.transform = `translate(${
+      //       Math.random() * (fieldWidth - bugWidth)
+      //     }px, ${Math.random() * (fieldHeight - bugHeight)}px)`;
+      //   });
+
+      //   field.classList.remove("invisible");
+    } else if (
+      // 일시정지
+      playBtn.classList.contains("stop")
+    ) {
+      playBtn.classList.remove("stop");
+      playBtn.classList.add("start");
+      playBtn.innerHTML = `
+      <i class="fas fa-play"></i>
+      `;
+
+      clearInterval(timer);
+      resultTitle.textContent = "REPLAY?";
+      resultContainer.classList.remove("invisible");
+    } else if (
+      //   일시정지에서 재개
+
+      playBtn.classList.contains("start") &&
+      !playBtn.classList.contains("init")
+    ) {
+      resultContainer.classList.add("invisible");
+      playBtn.classList.remove("start");
+      playBtn.classList.add("stop");
+
+      playBtn.innerHTML = `
+      <i class="fas fa-stop"></i>
+      `;
+
+      timer = setInterval(() => {
+        if (time === 0) {
+          clearInterval(timer);
+          playBtn.classList.add("invisible");
+          resultTitle.textContent = "YOU LOSE";
+          resultContainer.classList.remove("invisible");
+          playBtn.classList.add("init");
+        }
+        timeCounter.textContent = `0:${time--}`;
+      }, 1000);
+    }
+  });
+
+  // replayBtn 이벤트
+  replayBtn.addEventListener("click", () => {
+    initSet();
+    // //   class 세팅
+    // playBtn.classList.remove("invisible");
+    // resultContainer.classList.add("invisible");
+    // playBtn.classList.remove("start");
+    // playBtn.classList.add("stop");
+    // // html 세팅
+    // playBtn.innerHTML = `
+    //       <i class="fas fa-stop"></i>
+    //       `;
+    // // timeCounter 세팅
+    // time = 10;
+    // timeCounter.textContent = `0:${time--}`;
+    // timer = setInterval(() => {
+    //   if (time === 0) {
+    //     clearInterval(timer);
+    //     playBtn.classList.add("invisible");
+    //     resultTitle.textContent = "YOU LOSE";
+    //     resultContainer.classList.remove("invisible");
+    //     playBtn.classList.add("init");
+    //   }
+    //   timeCounter.textContent = `0:${time--}`;
+    // }, 1000);
+    // //   numCounter 세팅
+    // num = 10;
+    // numCounter.textContent = `${num}`;
+    // //   당근, 벌레 재배치
+    // const fieldWidth = field.offsetWidth;
+    // const fieldHeight = field.offsetHeight;
+    // const carrotWidth = carrot.width;
+    // const carrotHeight = carrot.height;
+    // const bugWidth = bug.width;
+    // const bugHeight = bug.height;
+    // carrots.forEach(carrot => {
+    //   carrot.style.transform = `translate(${
+    //     Math.random() * (fieldWidth - carrotWidth)
+    //   }px, ${Math.random() * (fieldHeight - carrotHeight)}px)`;
+    //   carrot.style.display = "inline";
+    // });
+    // bugs.forEach(bug => {
+    //   bug.style.transform = `translate(${
+    //     Math.random() * (fieldWidth - bugWidth)
+    //   }px, ${Math.random() * (fieldHeight - bugHeight)}px)`;
+    // });
   });
 
   // 당근, 벌레에 click 이벤트 추가
@@ -187,8 +237,7 @@ addEventListener("load", () => {
         resultContainer.classList.remove("invisible");
         playBtn.classList.add("init");
       }
-    }
-    if (event.target.classList.contains("bug")) {
+    } else if (event.target.classList.contains("bug")) {
       clearInterval(timer);
       playBtn.classList.add("invisible");
       resultTitle.textContent = "YOU LOSE";
